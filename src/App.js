3b38410071b5a897def874ed85029f82de7ea67c
Jeffry Almonte Dely <djempsly120@gmail.com>
2024-05-01 12:50:56 -0400

    Agregando los primeros componentes del archivo de registro

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Registrar } from './Registro';
+
+function App() {
+  return (
+  <>
+  <Registrar />
+  
+  </>
+  
+  );
+}
+
+export default App;
